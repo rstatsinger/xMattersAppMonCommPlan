ae070a308dcdf27f08f5951b2518a78e93f19c44
Travis DePuy <tdepuy@xmatters.com>
2017-07-04 12:05:58 -0500

    Removed commented out, sample code

--- InboundFromAppmon.js
+++ InboundFromAppmon.js
@@ -6,17 +6,6 @@ var data;
 
 // If your data is posted as JSON
 data = JSON.parse(request.body);
-  
-// If your data is posted as query string parameters or form post body
-// data = request.parameters;
-
-// If your data is posted as xml
-// data = JXON.parse(request.body)
-
-// Parse data from incoming payload and construct the trigger object
-// trigger.properties.Impact = data.Importance;
-// trigger.properties.Summary = data.Summary;
-// trigger.properties['Incident ID'] = data['Incident ID'];
 
 trigger.properties.message = data.properties[0].text;
 trigger.properties.subject = data.properties[0].subject;
@@ -26,19 +15,6 @@ trigger.properties.systemProfile = data.properties[0].systemProfile;
 trigger.properties.incidentKey = data.properties[0].incidentKey;
 trigger.properties.incidentID = data.properties[0].incidentID;
 trigger.properties.appMonServer = data.properties[0].appMonServer;
-
-// Define recipients
-//var recipients = [];
-// Add a recipient targeting a user or group
-//recipients.push({
-//    'targetName': 'Operations'
-//});
-// Add a recipient targeting specific devices
-//recipients.push({
-//    'targetName': 'bsmith',
-//    'devices': ['Work Email', 'Work Phone']
-//});
-///trigger.recipients = recipients;
 
 // Post trigger to form
  form.post(trigger);
